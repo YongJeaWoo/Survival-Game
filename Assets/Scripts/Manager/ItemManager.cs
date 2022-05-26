cc3e0ddf6c27cb544d2850661a8cd409a3285e9c
YongJeaWoo <utr.pbc@gmail.com>
2022-05-26 21:46:00 +0900

    테스트 9 - 2
    아이템 생성 시 같은 자리 생성 방지 구현 중

--- Assets/Scripts/Manager/ItemManager.cs
+++ Assets/Scripts/Manager/ItemManager.cs
@@ -28,6 +28,16 @@ public class ItemManager : MonoBehaviour
             {
                 int ranZone = Random.Range(0, 8);
                 Instantiate(items[itemList[0]], itemZones[ranZone].position, itemZones[ranZone].rotation);
+
+                //if (itemZones[ranZone] != null)
+                //    ranZone += 1;
+
+                //if (ranZone > itemZones.Length)
+                //    yield return null;
+
+                //if (itemZones[ranZone] )
+
+
                 itemList.RemoveAt(0);
                 yield return new WaitForSeconds(10f);
             }
